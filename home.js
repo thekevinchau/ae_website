772d7bd578ceed6d655a08633488d79de72ad08e
KEVIN CHAU <kevinch@udel.edu>
2024-01-03 18:33:05 -0500

    Removed html rendering and instead used JS to dynamically render image cards based on photo array length

--- home.js
+++ home.js
@@ -14,12 +14,23 @@ function setServiceColor() {
 
 //Function that generates the photos in order from the photo array.
 function setImages() {
+
     let photos = [
         "portfolio-photos/50da1426fc747415a57321c7b929643d.jpeg", 
         "portfolio-photos/8687fe95e58647c5fbea18566b2d564a.jpeg", 
         "portfolio-photos/017dfa9c6553b51df5e660a7369eecd6.jpeg",
         "portfolio-photos/1f0045823120765eb11e56ff989e9a59.jpeg"]
 
+    let portfolio_container = document.querySelector('.portfolio-cards')
+
+    for (let k = 0; k < photos.length; k++){
+        let photo_card = document.createElement('div');
+        let photo = document.createElement('div');
+        photo_card.className = "portfolio-card";
+        photo.className = "portfolio-image"
+        photo_card.appendChild(photo);
+        portfolio_container.appendChild(photo_card);
+    }
     let portfolio_images = document.querySelectorAll('.portfolio-image')
 
     for (let i = 0; i < portfolio_images.length; i++){
