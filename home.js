475564a7b73608260faf8d69571cf21938a1722f
KEVIN CHAU <kevinch@udel.edu>
2024-01-03 17:56:05 -0500

    Added looping functionality to import photos via relative path to photo cards

--- home.js
+++ home.js
@@ -1,8 +1,9 @@
-
+//photo array
+let photos = ["portfolio-photos/50da1426fc747415a57321c7b929643d.jpeg"]
 //Document Selectors
 let service_img = document.querySelectorAll('.service-image');
 let service_tiles = document.querySelectorAll('.service-tiles');
-
+let portfolio_images = document.querySelectorAll('.portfolio-image')
 //
 service_img.forEach((image) => {
     if (image.textContent.toLowerCase() === "e"){
@@ -11,5 +12,11 @@ service_img.forEach((image) => {
     else if (image.textContent.toLowerCase() === "p"){
         image.style.backgroundColor = "red";
     }})
-console.log(service_img);
 
+
+//Function that generates the photos in order from the photo array.
+for (let i = 0; i < portfolio_images.length; i++){
+    for (let j = 0; j < photos.length; j++){
+        portfolio_images[i].style.backgroundImage = "url(" + photos[i] + ")";
+    }
+}
